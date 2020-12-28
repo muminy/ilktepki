7e5f93db3cbd71e27b115e75c3f3e57af499d640
Mümin Yıldırım <mmnyldrm61@gmail.com>
2020-12-29 00:05:45 +0300

    spin

--- pages/thread/[id]/[slug].js
+++ pages/thread/[id]/[slug].js
@@ -7,7 +7,7 @@ import Cookies from "js-cookie";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import GetTiming from "helpers/getTime";
-import { CommentIcon, LikeIcon } from "@constants/icons";
+import { CommentIcon, LikeIcon, SpinIcon } from "@constants/icons";
 import ContentEditable from "react-contenteditable";
 import Comment from "@components/ui/Comment";
 import { useAuthToken } from "context/AuthToken";
@@ -113,26 +113,7 @@ export default function Slug({ post, id }) {
                 >
                   {sendCommentLoading ? (
                     <>
-                      <svg
-                        className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                      >
-                        <circle
-                          className="opacity-25"
-                          cx="12"
-                          cy="12"
-                          r="10"
-                          stroke="currentColor"
-                          strokeWidth="4"
-                        ></circle>
-                        <path
-                          className="opacity-75"
-                          fill="currentColor"
-                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                        ></path>
-                      </svg>
+                      <SpinIcon />
                       Yorum Gönderiliyor
                     </>
                   ) : (
@@ -183,26 +164,7 @@ export default function Slug({ post, id }) {
 const CommentsLoadingCard = () => (
   <div className="items-center text-center bg-gray-100 py-10 rounded-md">
     <div className="mx-auto w-5 mb-4">
-      <svg
-        className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          className="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="black"
-          strokeWidth="4"
-        ></circle>
-        <path
-          className="opacity-75"
-          fill="black"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
+      <SpinIcon color="#000000" />
     </div>
     <div className="font-semibold text-sm">Yorumlar yükleniyor... Lütfen bekleyiniz.</div>
   </div>
